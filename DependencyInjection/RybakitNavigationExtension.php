aeceb8a6f152e6963f93dc04f5bbff668c056ede
Eugene Leonovich <gen.work@gmail.com>
2012-12-07 14:55:35 +0100

    Fix CS

--- DependencyInjection/RybakitNavigationExtension.php
+++ DependencyInjection/RybakitNavigationExtension.php
@@ -2,10 +2,10 @@
 
 namespace Rybakit\Bundle\NavigationBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class RybakitNavigationExtension extends Extension
 {
