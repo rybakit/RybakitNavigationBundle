3d2317cae58f2bfcd85dde079f7f56f48ea6c009
Eugene Leonovich <gen.work@gmail.com>
2012-12-07 10:41:17 +0100

    Change tree traversal mode to post-order

--- Tests/Navigation/ItemFactoryTest.php
+++ Tests/Navigation/ItemFactoryTest.php
@@ -9,41 +9,34 @@ class ItemFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testCreate()
     {
+        $traversalIndex = 0;
+
         $filter = $this->getMock('\\Rybakit\\Bundle\\NavigationBundle\\Navigation\\Filter\\FilterInterface');
         $filter->expects($this->any())
             ->method('apply')
-            ->will($this->returnCallback(function($options, Item $item) {
+            ->will($this->returnCallback(function($options, Item $item) use (&$traversalIndex) {
                 $item->label = $options['label'];
+                $item->__index = $traversalIndex++;
             }));
 
         $factory = new ItemFactory($filter, new Item());
 
         $nav = $factory->create(array(
-            'label'     => '0',
-            'children'  => array(
-                array(
-                    'label' => '1.1',
+                'label'     => '0',
+                'children'  => array(
+                    array('label' => '1.1'),
+                    array('label' => '1.2', 'children'  => array(array('label' => '1.2.1'))),
+                    array('label' => '1.3'),
                 ),
-                array(
-                    'label'     => '1.2',
-                    'children'  => array(
-                        array(
-                            'label' => '1.2.1',
-                        ),
-                    ),
-                ),
-                array(
-                    'label' => '1.3',
-                ),
-            ),
-        ));
+            ));
 
-        $this->assertEquals('(0)(1.1)(1.2)(1.2.1)(1.3)', $this->dumpItem($nav));
+        // post-order traversal
+        $this->assertEquals('(4:0)(0:1.1)(2:1.2)(1:1.2.1)(3:1.3)', $this->dumpItem($nav));
     }
 
     protected function dumpItem(Item $item)
     {
-        $dump = '('.$item->label.')';
+        $dump = sprintf('(%d:%s)', $item->__index, $item->label);
 
         foreach ($item as $child) {
             $dump .= $this->dumpItem($child);
