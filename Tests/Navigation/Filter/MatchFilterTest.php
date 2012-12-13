d697edddb9024ad02501662ff05008b20fb49b82
Eugene Leonovich <gen.work@gmail.com>
2012-12-13 15:05:40 +0100

    Cleanup code

--- Tests/Navigation/Filter/MatchFilterTest.php
+++ Tests/Navigation/Filter/MatchFilterTest.php
@@ -11,26 +11,20 @@ class MatchFilterTest extends \PHPUnit_Framework_TestCase
      */
     public function testApply($isMatched)
     {
-        $item = $this->getMock('\\Rybakit\\Bundle\\NavigationBundle\\Navigation\\ItemInterface');
-
         $matcher = $this->getMock('\\Rybakit\\Bundle\\NavigationBundle\\Navigation\\Filter\\Matcher\\MatcherInterface');
-        $matcher->expects($this->once())->method('match')
-            ->will($this->returnValue($isMatched));
+        $matcher->expects($this->once())->method('match')->will($this->returnValue($isMatched));
 
         $options = array();
+        $item = $this->getMock('\\Rybakit\\Bundle\\NavigationBundle\\Navigation\\ItemInterface');
+
         $filter = new MatchFilter($matcher);
         $filter->apply($options, $item);
 
-        $isMatched
-            ? $this->assertEquals($item, $filter->getMatched())
-            : $this->assertNull($filter->getMatched());
+        $isMatched ? $this->assertEquals($item, $filter->getMatched()) : $this->assertNull($filter->getMatched());
     }
 
     public function testApplyProvider()
     {
-        return array(
-            array(true),
-            array(false),
-        );
+        return array(array(true), array(false));
     }
 }
