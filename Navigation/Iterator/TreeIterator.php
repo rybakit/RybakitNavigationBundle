4386823776573613655d0ab8de75fa62c7de8a51
Eugene Leonovich <gen.work@gmail.com>
2012-12-13 16:34:04 +0100

    Enhance TreeIterator

--- Navigation/Iterator/TreeIterator.php
+++ Navigation/Iterator/TreeIterator.php
@@ -4,8 +4,29 @@ namespace Rybakit\Bundle\NavigationBundle\Navigation\Iterator;
 
 class TreeIterator extends \IteratorIterator implements \RecursiveIterator
 {
+    /**
+     * @var int
+     */
+    protected $level;
+
+    /**
+     * @var int
+     */
+    protected $maxLevel = PHP_INT_MAX;
+
+    public function __construct(\Traversable $iterator, $level = 0)
+    {
+        parent::__construct($iterator);
+
+        $this->level = (int) $level;
+    }
+
     public function hasChildren()
     {
+        if ($this->level >= $this->maxLevel) {
+            return false;
+        }
+
         $iterator = $this->current();
 
         if ($iterator instanceof \IteratorAggregate) {
@@ -21,6 +42,22 @@ class TreeIterator extends \IteratorIterator implements \RecursiveIterator
 
     public function getChildren()
     {
-        return new static($this->current());
+        return new static($this->current(), $this->level + 1);
+    }
+
+    /**
+     * @param int $maxLevel
+     */
+    public function setMaxLevel($maxLevel)
+    {
+        $this->maxLevel = (int) $maxLevel;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel()
+    {
+        return $this->level;
     }
 }
